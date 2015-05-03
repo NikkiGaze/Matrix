bb1d8954e86a7d67e9cd8f0a67b35516c93ac8c9
NikkiG <alex_lay@mail.ru>
2015-05-03 22:48:59 +0300

    Добаыил глебу

--- Matrix/mainwindow.cpp
+++ Matrix/mainwindow.cpp
@@ -1,9 +1,21 @@
 #include "mainwindow.h"
+#include <QHBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    spinb = new QDoubleSpinBox(this);
+    spinb->setSingleStep(0.1);
+    QHBoxLayout * lt = new QHBoxLayout(this);
+    lt->addWidget(spinb);
+    setLayout(lt);
 }
+
+double MainWindow::getX()
+{
+    return spinb->value();
+}
+
 
 MainWindow::~MainWindow()
 {
--- Matrix/mainwindow.h
+++ Matrix/mainwindow.h
@@ -2,13 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDoubleSpinBox>
+
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+    QDoubleSpinBox * spinb;
 public:
     MainWindow(QWidget *parent = 0);
+    double getX();
     ~MainWindow();
 };
 
